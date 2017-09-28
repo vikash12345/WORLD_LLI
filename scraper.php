0d50c110d8379332b7f6f5a91e3bce2cbf91c99d
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-28 11:40:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,43 +7,10 @@ require 'scraperwiki/simple_html_dom.php';
 for($page = 0; $page < 150; $page+=50)
 {
 $url = 'http://www.worldlii.org/cgi-bin/sinosrch.cgi?method=auto;meta=%2Fworldlii;mask_path=;mask_world=;query=a;results=50;submit=Search;rank=on;callback=off;legisopt=;view=relevance;offset='.$page;
-function get_web_page( $url )
-    {
-        $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
-
-        $options = array(
-
-            CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
-            CURLOPT_POST           =>false,        //set to GET
-            CURLOPT_USERAGENT      => $user_agent, //set user agent
-            CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
-            CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
-            CURLOPT_RETURNTRANSFER => true,     // return web page
-            CURLOPT_HEADER         => false,    // don't return headers
-            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-            CURLOPT_ENCODING       => "",       // handle all encodings
-            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-            CURLOPT_TIMEOUT        => 120,      // timeout on response
-            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
-        );
-
-        $ch      = curl_init( $url );
-        curl_setopt_array( $ch, $options );
-        $content = curl_exec( $ch );
-        $err     = curl_errno( $ch );
-        $errmsg  = curl_error( $ch );
-        $header  = curl_getinfo( $ch );
-        curl_close( $ch );
-
-        $header['errno']   = $err;
-        $header['errmsg']  = $errmsg;
-        $header['content'] = $content;
-        return $header;
-    }
- $maincode = get_web_page( $url );
-    echo $maincode;
-
+$html = scraperwiki::scrape($url);
+$dom = new simple_html_dom();
+$dom->load($html);
+echo $dom;
 }
 
 
