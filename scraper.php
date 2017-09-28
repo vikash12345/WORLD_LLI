2abb7f9330d70ce97f248fa3cd0d94a2e70918c0
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-28 11:20:12 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,17 +7,14 @@ require 'scraperwiki/simple_html_dom.php';
 for($page = 0; $page < 100; $page+=50)
 {
   $NEWLINK = 'http://www.worldlii.org/cgi-bin/sinosrch.cgi?method=auto;meta=%2Fworldlii;mask_path=;mask_world=;query=a;results=50;submit=Search;rank=on;callback=off;legisopt=;view=relevance;offset='.$page;
-  $url = file_get_html($NEWLINK);
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_VERBOSE, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
-  curl_setopt($ch, CURLOPT_URL, urlencode($url));
+  curl_setopt($ch, CURLOPT_URL, urlencode($NEWLINK));
   $response = curl_exec($ch);
   curl_close($ch);
-  
-  echo $url;
 }
 
 
