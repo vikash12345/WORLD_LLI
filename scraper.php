132cf15c6dd881366bd5ee27e4862b888890e683
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-28 12:43:31 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,7 +7,17 @@ require 'scraperwiki/simple_html_dom.php';
 for($page = 0; $page < 20; $page+=20)
 {
 $url  = 'http://www.commonlii.org/cgi-bin/sinosrch.cgi?query=a;results=20;submit=Search;rank=on;callback=on;method=auto;meta=%2Fcommonlii;lii=CommonLII;offset='.$page;
-$html = file_get_html($url);
+  
+  
+  $curl_handle=curl_init();
+curl_setopt($curl_handle, CURLOPT_URL,$url);
+curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+$query = curl_exec($curl_handle);
+curl_close($curl_handle);
+  
+  
 foreach($html->find("//*[@id='view']/ol/li/p")as $element)
 {
   echo $element;
