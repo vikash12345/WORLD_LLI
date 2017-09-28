fc95747dcfcd29d14b5d03051af113e5a10bf698
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-28 11:33:18 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,45 +7,18 @@ require 'scraperwiki/simple_html_dom.php';
 for($page = 0; $page < 150; $page+=50)
 {
    $MyWebsite = 'http://www.worldlii.org/cgi-bin/sinosrch.cgi?method=auto;meta=%2Fworldlii;mask_path=;mask_world=;query=a;results=50;submit=Search;rank=on;callback=off;legisopt=;view=relevance;offset='.$page;
-	     $cHeadres = array(
-      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Language: en-US,en;q=0.5',
-      'Connection: Keep-Alive',
-      'Pragma: no-cache',
-      'Cache-Control: no-cache'
-     );
+	// create a new cURL resource
+$ch = curl_init();
 
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, $MyWebsite);
+curl_setopt($ch, CURLOPT_HEADER, 0);
 
+// grab URL and pass it to the browser
+curl_exec($ch);
 
- function dlPage($href) {
-  global $cHeadres;
-
-  $ch = curl_init();
-  if($ch){
-   curl_setopt($ch, CURLOPT_URL, $href);
-   curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
-   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
-   curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
-   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-   curl_setopt($ch, CURLOPT_HEADER, false);
-   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
-   $str = curl_exec($ch);
-   curl_close($ch);
-
-   $dom = new simple_html_dom();
-   $dom->load($str);
-   return $dom;
-  }
- }
-
- $maincode = dlPage($MyWebsite);
- echo $maincode;
- 
-
- 
+// close cURL resource, and free up system resources
+curl_close($ch);
  
 
 }
