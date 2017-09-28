6f3236b101e8d1a4ef5337b0218feb25113619f1
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-28 11:43:01 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,11 +6,9 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 for($page = 0; $page < 150; $page+=50)
 {
-$url = 'http://www.worldlii.org/cgi-bin/sinosrch.cgi?method=auto;meta=%2Fworldlii;mask_path=;mask_world=;query=a;results=50;submit=Search;rank=on;callback=off;legisopt=;view=relevance;offset='.$page;
-$html = scraperwiki::scrape($url);
-$dom = new simple_html_dom();
-$dom->load($html);
-echo $dom;
+$url  = 'http://www.worldlii.org/cgi-bin/sinosrch.cgi?method=auto;meta=%2Fworldlii;mask_path=;mask_world=;query=a;results=50;submit=Search;rank=on;callback=off;legisopt=;view=relevance;offset='.$page;
+$html = file_get_html($url
+                      echo $html;
 }
 
 
