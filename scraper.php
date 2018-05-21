389d40a0a0e3e3fd72750c5764594cb87307a3f2
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-05-21 08:33:14 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //5369780
-for($page = 295460; $page < 5369780; $page+=20)
+for($page = 311020; $page < 5369780; $page+=20)
 {
   
   echo "$page\n";
